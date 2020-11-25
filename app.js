8c8ddf38f0fb16052702d800cab2840acd981110
malbannai <moudhi.albannai@gamil.com>
2020-11-25 20:02:16 +0300

    Day 3 backend, DB setup done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser"); //bodyParser
+const db = require("./db/models");
 
 const app = express();
 const itemsRoutes = require("./routes/items");
@@ -9,4 +10,16 @@ app.use(cors());
 app.use(bodyParser.json()); //bodyParser
 app.use("/items", itemsRoutes);
 
-app.listen(8000);
+const run = async () => {
+  try {
+    await db.sequelize.authenticate();
+    console.log("Connection to the database successful!");
+    await app.listen(8000, () => {
+      console.log("The application is running on localhost:8000");
+    });
+  } catch (error) {
+    console.error("Error connecting to the database: ", error);
+  }
+};
+
+run();
